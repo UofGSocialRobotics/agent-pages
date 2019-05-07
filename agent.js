c3e236872fa9bfbbea76fbdbdbf1f044cf79c053
lucile <lucile.callebert@gmail.com>
2019-05-07 17:15:27 +0100

    conncting to broker and posting msg

--- agent.js
+++ agent.js
@@ -1,5 +1,42 @@
+// SETTINGS
+var BROKER = "ws://iot.eclipse.org/ws";
+var TOPIC = "UoGSocialRobotics/ConversationalAgent/m_ASR";
 
-def response_fct(){
-	var v = document.getElementById("textfield").value;
-	document.getElementById("response").value = v+" is not nice!"
+var mqtt;
+var clientID;
+var reconnectTimeout = 2000;
+// var host = "broker.mqttdashboard.com";
+// var host = "iot.eclipse.org"
+// var port = 1883;
+
+function onConnect(){
+	//Once a connection has been made, make a subsrcription and send a message
+	console.log("Connected");
+	var message = new Paho.MQTT.Message(clientID+": new client connected: ");
+	message.destinationName = TOPIC;
+	mqtt.send(message);
+}
+
+function MQTTConnect(){
+	console.log("Connecting to "+BROKER);
+	clientID = "UofGConversationalAgent" + new Date().getTime();
+	mqtt = new Paho.MQTT.Client(BROKER, clientID);
+	var options = { timeout: 30, onSuccess: onConnect,};
+	mqtt.connect(options);
+}
+
+
+function MQTTSendMessage(){
+	//Get message
+	var msg = document.getElementById("textfield").value;
+
+	// Thank user for message
+	var resp_div = document.getElementById("response");
+	var txt = "<p>Thanks for your message to the world.</p>";
+	resp_div.innerHTML = txt;
+
+	// Send message to broker
+	var message = new Paho.MQTT.Message(clientID+": "+msg);
+	message.destinationName = TOPIC;
+	mqtt.send(message);
 }
