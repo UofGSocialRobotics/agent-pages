c4e5115ebc9048542872b8e4fd0e810256b372bd
LucileC <lucile.callebert@gmail.com>
2019-05-13 13:19:45 +0100

    basic functionning architecture

--- agent.js
+++ agent.js
@@ -1,6 +1,8 @@
 // SETTINGS
 var BROKER = "wss://iot.eclipse.org/ws";
-var TOPIC = "UoGSocialRobotics/ConversationalAgent/m_ASR";
+var MAIN_TOPIC = "UoGSocialRobotics/ConversationalAgent/"
+var TOPIC_PUBLISH = MAIN_TOPIC+"Client/";
+var TOPIC_SUBSCRIBE = MAIN_TOPIC + "Server_out/"
 
 var mqtt;
 var clientID;
@@ -9,18 +11,25 @@ var reconnectTimeout = 2000;
 // var host = "iot.eclipse.org"
 // var port = 1883;
 
+function updateTopicSubscribe(client_id){
+	TOPIC_SUBSCRIBE += client_id
+}
+
 function onConnect(){
 	//Once a connection has been made, make a subsrcription and send a message
 	console.log("Connected");
 	var message = new Paho.MQTT.Message(clientID+": new client connected: ");
-	message.destinationName = TOPIC;
+	message.destinationName = TOPIC_PUBLISH;
 	mqtt.send(message);
+	mqtt.subscribe(TOPIC_SUBSCRIBE)
 }
 
 function MQTTConnect(){
 	console.log("Connecting to "+BROKER);
-	clientID = "UofGConversationalAgent" + new Date().getTime();
+	clientID = "WebClient" + new Date().getTime();
+	updateTopicSubscribe(clientID)
 	mqtt = new Paho.MQTT.Client(BROKER, clientID);
+	mqtt.onMessageArrived = onMessageArrived;
 	var options = { timeout: 30, onSuccess: onConnect,};
 	mqtt.connect(options);
 }
@@ -37,6 +46,11 @@ function MQTTSendMessage(){
 
 	// Send message to broker
 	var message = new Paho.MQTT.Message(clientID+": "+msg);
-	message.destinationName = TOPIC;
+	message.destinationName = TOPIC_PUBLISH;
 	mqtt.send(message);
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
 }
