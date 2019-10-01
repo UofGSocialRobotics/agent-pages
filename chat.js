bccbe77cd30fe623fbc688993e3791aa6b59e36f
LucileC <lucile.callebert@gmail.com>
2019-10-01 14:13:28 +0100

    website working up to chat

--- chat.js
+++ chat.js
@@ -91,21 +91,26 @@ var app_global = {
         piece_of_data: false,
         text: false,
         dialog_stated: false,
-    }
+    },
+    answers_demographics : {},
+    answer_demographics_weight_kg : undefined,
+    answer_demographics_height_cm : undefined
 };
 
 var PAGES = {
     AMTID : "amtid.html",
     INTRO : "intro.html",
     INSTRUCTIONS : "instructions.html",
+    DEMOGRPAHICS : "demographics.html",
     PRE_STUDY_QUESTIONNAIRE : "pre_study_questionnaire.html",
     FOOD_DIAGNOSIS : "food_diagnosis.html",
     CHAT_SETUP : "chat_setup.html",
     CHAT : "chat.html",
     QUESTIONNAIRE : "questionnaire.html",
     THANKS : "thanks.html",
+    DEMOGRPAHICS : "demographics.html"
 }
-var PAGES_SEQUENCE = [PAGES.AMTID, PAGES.INTRO, PAGES.INSTRUCTIONS, PAGES.FOOD_DIAGNOSIS, PAGES.CHAT_SETUP, PAGES.CHAT, PAGES.QUESTIONNAIRE, PAGES.THANKS];
+var PAGES_SEQUENCE = [PAGES.AMTID, PAGES.INTRO, PAGES.INSTRUCTIONS, PAGES.DEMOGRPAHICS, PAGES.FOOD_DIAGNOSIS, PAGES.CHAT_SETUP, PAGES.CHAT, PAGES.QUESTIONNAIRE, PAGES.THANKS];
 
 var MSG_TYPES = {
     INFO : 'info',
@@ -177,7 +182,8 @@ FIREBASE_KEYS = {
     PRESTUDYANSWERS : "pre_study_questionnaire_answers",
     SOURCE : "source",
     TEXT : "text",
-    FOODDIAGNOSISANSWERS : "food_diagnosis_answers"
+    FOODDIAGNOSISANSWERS : "food_diagnosis_answers",
+    DEMOGRPAHICS : "demographics"
 };
 
 FIREBASE_VALUES = {
@@ -200,6 +206,7 @@ data_col[FIREBASE_KEYS.CLIENTID] = app_global.clientID;
 data_col[FIREBASE_KEYS.PRESTUDYANSWERS] = false;
 data_col[FIREBASE_KEYS.POSTSTUDYANSWERS] = false;
 data_col[FIREBASE_KEYS.FOODDIAGNOSISANSWERS] = false;
+data_col[FIREBASE_KEYS.DEMOGRPAHICS] = false;
 FIREBASE_SESSION_STRUCTURE[FIREBASE_KEYS.DATACOLLECTION] = data_col;
 FIREBASE_SESSION_STRUCTURE[FIREBASE_KEYS.DIALOG] = {};
 FIREBASE_SESSION_STRUCTURE[FIREBASE_KEYS.DIALOG][FIREBASE_KEYS.CLIENTID] = app_global.clientID;
@@ -253,6 +260,7 @@ function on_load(){
     if (page == PAGES.THANKS) amt_validation_code();
     if (page == PAGES.PRE_STUDY_QUESTIONNAIRE) create_pre_study_questionnaire();   
     if (page == PAGES.FOOD_DIAGNOSIS) create_food_diagnosis_questionnaire();
+    if (page == PAGES.DEMOGRPAHICS) init_demogrpahics();
 	window.speechSynthesis.onvoiceschanged = function() {
         app_global.voices=window.speechSynthesis.getVoices();
     };
@@ -451,7 +459,7 @@ function check_ack(callback){
     // return check_ack_listner(FIREBASE_REFS.CURRENT_SESSION.child(FIREBASE_KEYS.ACK));
     var cond = FIREBASE_REFS.CURRENT_SESSION.child(FIREBASE_KEYS.ACK).once('value').then(function(snapshot){
         console.log(snapshot.val());
-        console.log("we are here");
+        // console.log("we are here");
         if (snapshot.val() == true) {
             var data = {};
             data[FIREBASE_KEYS.ACK] = false;
@@ -591,22 +599,28 @@ function get_page_name(){
 }
 
 function go_to_intro(){
-    location.replace(PAGES.INTRO+"?clientid="+app_global.clientID)
+    location.replace(PAGES.INTRO+"?clientid="+app_global.clientID);
 }
 function go_to_instructions(){
-    location.replace(PAGES.INSTRUCTIONS+"?clientid="+app_global.clientID)
+    location.replace(PAGES.INSTRUCTIONS+"?clientid="+app_global.clientID);
+}
+function go_to_demographics(){
+    location.replace(PAGES.DEMOGRPAHICS+"?clientid="+app_global.clientID);
+}
+function go_to_food_diagnosis(){
+    location.replace(PAGES.FOOD_DIAGNOSIS+"?clientid="+app_global.clientID);
 }
 function go_to_pre_study_questionnaire(){
-    location.replace(PAGES.PRE_STUDY_QUESTIONNAIRE+"?clientid="+app_global.clientID)
+    location.replace(PAGES.PRE_STUDY_QUESTIONNAIRE+"?clientid="+app_global.clientID);
 }
 function go_to_chat_setup(){
-    location.replace(PAGES.CHAT_SETUP+"?clientid="+app_global.clientID)
+    location.replace(PAGES.CHAT_SETUP+"?clientid="+app_global.clientID);
 }
 function go_to_chat(tts_asr){
-    location.replace(PAGES.CHAT+"?clientid="+app_global.clientID+"?tts_asr="+tts_asr.toString())
+    location.replace(PAGES.CHAT+"?clientid="+app_global.clientID+"?tts_asr="+tts_asr.toString());
 }
 function go_to_questionnaire(x){
-    location.replace(PAGES.QUESTIONNAIRE+"?clientid="+app_global.clientID+"?q_id="+x)
+    location.replace(PAGES.QUESTIONNAIRE+"?clientid="+app_global.clientID+"?q_id="+x);
 }
 function go_to_page_after_questionnaire(){
     if (get_page() == PAGES.PRE_STUDY_QUESTIONNAIRE) go_to_chat_setup();
@@ -632,16 +646,26 @@ function go_to_next_page(param){
                     go_to_instructions();
                     break;
                 case PAGES.PRE_STUDY_QUESTIONNAIRE:
+                    // console.log("go_to_pre_study_questionnaire");
                     go_to_pre_study_questionnaire();
                     break;
                 case PAGES.CHAT_SETUP:
                     go_to_chat_setup();
+                    break;
+                case PAGES.DEMOGRPAHICS:
+                    go_to_demographics();
+                    break;
+                case PAGES.FOOD_DIAGNOSIS:
+                    go_to_food_diagnosis();
                     break;
                 default:
                     console.log("ERROR: not implemented for "+next_page);
                     break;                
             }
         }
+    }
+    else{
+        console.log("Can't find current page, can't move to next one!!");
     }
 }
 
@@ -1056,7 +1080,7 @@ function create_likert_scale(q_id, q_text, text_labels=true, label_begin="totall
     }
     html += "\n<ul class='"+class_style+"'>";
     for (i = 0; i < n_points; i++) {
-        console.log(i, n_points);
+        // console.log(i, n_points);
         if (i == 0) html += "\n<li><input type=\"radio\" name=\"likert_"+q_id+"\" value=\""+q_id+"_"+i+"\"><label>"+label_begin+"</label>\n</li>";
         else if (i == n_points-1) {
             html += "\n<li><input type=\"radio\" name=\"likert_"+q_id+"\" value=\""+q_id+"_"+i+"\"><label>"+label_end+"</label>\n</li>\n</ul>";
@@ -1124,4 +1148,209 @@ function amt_validation_code(){
     var amt_div = document.getElementById("AMT_validation_code");
     var code = "VAL"+app_global.clientID;
     amt_div.innerHTML = code;
+}
+
+//--------------------------------------------------------------------------------------------------------------//
+//--------                                    DEMOGRAPHICS QUESTIONNAIRE                                --------//
+//--------------------------------------------------------------------------------------------------------------//
+
+
+
+var slider = document.getElementById("height_cm_input");
+var output = document.getElementById("height_cm_output");
+
+function init_demogrpahics(){
+    // Height
+    output.innerHTML = "    cm";
+    slider.oninput = function() {
+        output.innerHTML = this.value + " cm";
+        app_global.answers_demographics["height_unit_cm"] = this.value;
+        app_global.answer_demographics_height_cm = this.value;
+    }
+    var feet_input = document.getElementById("height_unit_feet");
+    var inches_input = document.getElementById("height_unit_in");
+    feet_input.oninput = function() {
+        var feet = this.value;
+        convert_to_cm(feet, inches_input.value);
+    }
+    inches_input.oninput = function() {
+        var inches = this.value;
+        convert_to_cm(feet_input.value, inches);
+        console.log(app_global.answers_demographics);
+    }
+    // Weight
+    var radio_height_unit_cm = document.getElementById("height_unit_cm");
+    var radio_height_unit_feet_in = document.getElementById("height_unit_feet_in");
+    var div_height_feet_in = document.getElementById("div_height_feet_in");
+    var div_heignt_cm = document.getElementById("div_heignt_cm");
+    radio_height_unit_cm.onclick = function() {
+        if (radio_height_unit_cm.checked){
+            div_heignt_cm.style = "display:block;"
+            div_height_feet_in.style = "display:none;"
+        }
+        else {
+            div_heignt_cm.style = "display:none;"
+            div_height_feet_in.style = "display:block;"
+        }
+    }
+    radio_height_unit_feet_in.onclick = function() {
+        if (radio_height_unit_feet_in.checked){
+            div_heignt_cm.style = "display:none;"
+            div_height_feet_in.style = "display:block;"
+        }
+        else {
+            div_heignt_cm.style = "display:block;"
+            div_height_feet_in.style = "display:none;"
+        }
+    }
+
+    var weight_kg_input = document.getElementById("weight_kg_input");
+    var weight_kg_output = document.getElementById("weight_kg_output");
+    weight_kg_output.innerHTML = "    kg";
+    weight_kg_input.oninput = function() {
+        weight_kg_output.innerHTML = this.value + " kg";
+        app_global.answers_demographics["weight_unit_kg"] = this.value;
+        app_global.answer_demographics_weight_kg = this.value;
+    }
+
+    var weight_pounds_input = document.getElementById("weight_pounds_input");
+    var weight_pounds_output = document.getElementById("weight_pounds_output");
+    weight_pounds_output.innerHTML = "    pounds  (0 kg)";
+    weight_pounds_input.oninput = function() {
+        var pounds = this.value;
+        var kg = Math.round(pounds*0.453592);
+        weight_pounds_output.innerHTML = this.value + " pounds  (" + kg + " kg)";
+
+        app_global.answers_demographics["weight_unit_pounds"] = this.value;
+        app_global.answers_demographics["weight_unit_kg"] = kg;
+        app_global.answer_demographics_weight_kg = kg;
+    }
+
+    var weight_stones_input = document.getElementById("weight_stones_input");
+    var weight_stones_output = document.getElementById("weight_stones_output");
+    weight_stones_output.innerHTML = "    stones 0 lb";
+    weight_stones_input.oninput = function() {
+        var stones_decimal = this.value;
+        var stones_int = Math.floor(stones_decimal);
+        var lb = Math.floor((stones_decimal - stones_int) * 14);
+        var kg = Math.round(stones_decimal * 6.35029);
+        var stones_lb = stones_int + " stones " + lb + " lb"
+        weight_stones_output.innerHTML = stones_lb + "  (" +kg + " kg)";
+
+        app_global.answers_demographics["weight_unit_stones"] = this.value;
+        app_global.answers_demographics["weight_unit_stones_lb"] = stones_lb;
+        app_global.answers_demographics["weight_unit_kg"] = kg;
+        app_global.answer_demographics_weight_kg = kg;
+    }
+
+    var radio_weight_unit_kg = document.getElementById("weight_unit_kg");
+    var radio_weight_unit_pounds = document.getElementById("weight_unit_pounds");
+    var radio_weight_unit_stones = document.getElementById("weight_unit_stones");
+    var div_weight_unit_kg = document.getElementById("div_weight_kg");
+    var div_weight_unit_pounds = document.getElementById("div_weight_pounds");
+    var div_weight_unit_stones = document.getElementById("div_weight_stones");
+
+    radio_weight_unit_kg.onclick = function(){
+        onclick_weight_display(radio_weight_unit_kg, div_weight_unit_kg, div_weight_unit_pounds, div_weight_unit_stones);
+    }
+    radio_weight_unit_pounds.onclick = function(){
+        onclick_weight_display(radio_weight_unit_pounds, div_weight_unit_pounds, div_weight_unit_kg, div_weight_unit_stones);
+    }
+    radio_weight_unit_stones.onclick = function(){
+        onclick_weight_display(radio_weight_unit_stones, div_weight_unit_stones, div_weight_unit_kg, div_weight_unit_pounds);
+    }
+
+}
+
+
+function convert_to_cm(feet, inches){
+    console.log("in convert_to_cm");
+    console.log(feet);
+    console.log(inches);
+    if (feet != "" && inches != ""){
+        var cm = 30.48*feet + 2.54*inches;
+        app_global.answers_demographics["height_unit_cm"] = cm;
+        app_global.answers_demographics["height_unit_feet_in"] = feet.toString() + " feet " + inches.toString() + " inches";
+        app_global.answer_demographics_height_cm = cm;
+    }
+}
+
+function onclick_weight_display(radio_id, to_display, to_hide1, to_hide2){
+    console.log("in onclick_weight_display, radio_id = " + radio_id);
+    if (radio_id.checked){
+        to_display.style = "display:block;";
+        to_hide1.style = "display:none;"
+        to_hide2.style = "display:none;"
+    }
+}
+
+function get_demographics_answers(){
+    
+    var inputs = document.getElementsByTagName("input");
+    console.log(app_global);
+    app_global.answers_demographics["employment"] = [];
+    for (var i=0; i < inputs.length; i++){
+        var input = inputs[i];
+        var key = input.name;
+        if (input.type == "radio"){
+            if (!(key in app_global.answers_demographics)) app_global.answers_demographics[key] = undefined;
+            if (input.checked) {
+                app_global.answers_demographics[key] = input.value;
+            }
+        }
+        else if (input.type == "checkbox"){
+            if (input.checked){
+                // console.log(input.value + " is checked!");
+                app_global.answers_demographics[key].push(input.value);
+            }
+        }
+        else if (input.type != "range"){
+            app_global.answers_demographics[key] = input.value;
+        }
+        // check answers
+        if (i == (inputs.length - 1)) {
+            check_answers_demographics();
+        }
+    }
+}
+
+function check_answers_demographics(){
+    console.log(app_global.answers_demographics);
+    var alert_bool = false;
+    for (var j in app_global.answers_demographics){
+        var label = document.getElementById("label_"+j);
+        if (app_global.answers_demographics[j] == undefined || app_global.answers_demographics[j]=="") {
+            alert_bool = true;
+            label.style = "color:red;font-weight:bold;"
+        }
+        else {
+            if (label == null) console.log("Cannot cahge style of "+ j+" as it does not exist!");
+            else label.style = "";
+        }
+        if (j == "weight_unit" || j == "height_unit"){
+            if (app_global.answers_demographics[j] == "feet_in"){
+                console.log("Check for feet and inches")
+            }
+            else {
+                if (app_global.answers_demographics[j]!=undefined){
+                    var key_to_check_for = j+"_"+app_global.answers_demographics[j];
+                    var label2 = document.getElementById("label_"+key_to_check_for);
+                    if (!(app_global.answers_demographics[key_to_check_for])){
+                        alert_bool = true;
+                        label2.style = "color:red;font-weight:bold;"
+                    }
+                    else {
+                        if (label2 == null) console.log(j);
+                        label2.style = "";
+                    }
+                }
+            }
+        }
+    }
+    if (alert_bool == true) {
+        console.log("alert");
+        window.scrollTo(0,0);
+        alert("You must answer all the questions");
+    }
+    else send_data_collection(app_global.answers_demographics, FIREBASE_KEYS.DEMOGRPAHICS);
 }
